990107caf21f47e2ab4b2607c7c238cd977ea480
manu-HP <manumcasas@gmail.com>
2024-08-17 16:24:44 +0200

    running with schedule 9am

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import schedule
-## import time
+import time
 import pronosticos
 import os
 
@@ -25,13 +25,13 @@ def main():
     # Programa las llamadas a las funciones en intervalos diferentes
     #schedule.every(1).hour.do(pronosticos.obtener_pronostico_actual, api_key=api_key, city=city)
     #schedule.every(3).hours.do(pronosticos.obtener_pronostico_corto_plazo, api_key=api_key, city=city)
-    schedule.every().day.at("09:05").do(pronosticos.obtener_pronostico_futuro, api_key=api_key, city=city)
+    schedule.every().day.at("07:05").do(pronosticos.obtener_pronostico_futuro, api_key=api_key, city=city)
     outputPronostico=pronosticos.obtener_pronostico_futuro(api_key=api_key,city=city)
 
     bot_send_msg(botToken,5178063489,outputPronostico)
-    #while True:
-    #    schedule.run_pending()
-    #    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
